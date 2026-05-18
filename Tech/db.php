6af43fe88a750e4872579cdf77559a0f4a7d510d
Shinthea Akter Eti <shintheaaktereti@gmail.com>
2026-05-18 18:43:34 +0600

    remove old file from main

--- Tech/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getConnection(){
-    $con = mysqli_connect("localhost", "root", "", "shop_db");
-    if(!$con){
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    return $con;
-}
--- Tech/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('location: view/home.php');
-exit;
